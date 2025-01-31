0c0c09a3f79b6a1afba9c99530d8d7df5c7c252a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-01-31 16:41:49 +0100

    Migrate to official cask

--- Casks/freelens.rb
+++ Casks/freelens.rb
@@ -1,15 +1,14 @@
 cask "freelens" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.1.3"
-  sha256 arm:   "a60ab1700dae51eccfd2dc007fba2431a1f34d83e312630ff889074467b8c9fe",
-         intel: "56251b6496d2145b4b3fa5001c00866a66398bad65a149ce4dcc61fd6d6f073d"
-
-  url "https://github.com/freelensapp/freelens/releases/download/v#{version}/Freelens-#{version}-macos-#{arch}.dmg",
-      verified: "github.com/freelensapp/freelens/"
+  version "0.1.3.1"
   name "Freelens"
   desc "Kubernetes IDE"
   homepage "https://freelens.app/"
+
+  def install
+    odie "This formula has been moved to Homebrew Cask. Please migrate from this private tap using:\n\nbrew untap --force freelensapp/tap\nbrew install --cask freelens"
+  end
 
   depends_on macos: ">= :high_sierra"
 
--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 Homebrew tap for [Freelens](https://github.com/freelensapp/freelens).
 
-Usage:
+## Warning
+
+This formula has been moved to Homebrew Cask. See: <https://formulae.brew.sh/cask/freelens#default>
+
+Please migrate from this private tap using:
 
 ```sh
-brew tap freelensapp/tap
+brew untap --force freelensapp/tap
 brew install --cask freelens
 ```
